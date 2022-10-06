38af5db6f51910c237b6b6e9c508dfb490a61257
Najmus Sakib <92445715+najmusskb@users.noreply.github.com>
2022-10-06 20:52:24 +0600

    router

---
+++ src/steps.js
@@ -0,0 +1,12 @@
+/* 
+
+1. install npm i react-router-dom 
+2. create a router using create browser route
+3.Add router provider and pass router props
+4.create some route (kon rastay gele ki ki dekhabe seita desfine kore dekhate hobe )
+
+
+eita diye search deiba 
+http://localhost:3000/about
+http://localhost:3000/home
+*/
